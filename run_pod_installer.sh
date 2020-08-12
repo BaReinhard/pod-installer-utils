ebfc54fb9fcf31f0a84850eda5ef86fce2ff091c
brett.reinhard <brett.reinhard@ticketmaster.com>
2020-08-11 17:40:41 -0700

    Added ntlm-auth support through pip3, this solves the downloading file issues

---
+++ run_pod_installer.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if test -f "pathofdiablolauncher.msi" 
+then
+    echo "PoD Launcher Installer exists, moving on."
+else
+    wget https://pathofdiablo.com/pathofdiablolauncher.msi
+fi
+
+WINEARCH=win32 WINEPREFIX=~/.wine wine msiexec /i ./pathofdiablolauncher.msi
+
+https://cdn.discordapp.com/attachments/602662727961542696/725687862103769109/patch_d2.mpq
